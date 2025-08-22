71dd7a4a6659c5ea4977f9d0c4d70fbbd516bd49
JuanCaING20072002 <juan.galeanob@campusucc.edu.co>
2025-08-22 15:23:44 -0500

    Aumenta tamaño de password_hash a 256 caracteres para evitar truncamiento

--- app.py
+++ app.py
@@ -54,7 +54,7 @@ class Viaje(db.Model):
 class Usuario(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128), nullable=False)
+    password_hash = db.Column(db.String(256), nullable=False)
     rol = db.Column(db.String(20), nullable=False, default='usuario')  # 'usuario' o 'admin'
 
     def set_password(self, password):
